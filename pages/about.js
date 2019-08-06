9ab14dea128e16365089e9ba28cd8eeb73339b12
Steve Holgado <steveholgado@gmail.com>
2019-08-06 19:43:28 +0100

    Minor formatting amends

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
 
-const About = () => (
-  <>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
-    <h1>About</h1>
-  </>
-)
+function About() {
+  return (
+    <>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <h1>About</h1>
+    </>
+  )
+}
 
 export default About
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
 
-const Index = () => (
-  <>
-    <Link href="/about">
-      <a>About</a>
-    </Link>
-    <h1>Home</h1>
-  </>
-)
+function Index() {
+  return (
+    <>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <h1>Home</h1>
+    </>
+  )
+}
 
 export default Index
